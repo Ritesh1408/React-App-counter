c48bbfeb0cf85ebae5c04abc3a14a20985cc6c78
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-05 15:12:31 +0530

    Updated build

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -20,7 +20,7 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<UserState>) => {
+    setUser: (_state, action: PayloadAction<UserState>) => {
       return action.payload;
     },
   },
